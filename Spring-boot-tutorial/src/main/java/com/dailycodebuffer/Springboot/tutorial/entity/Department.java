51cb43760ed7848cddfcca719ea988425dbdc95c
Girish <girishshewale8055@gmail.com>
2023-10-05 22:41:28 +0530

    first commit

---
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/entity/Department.java
@@ -0,0 +1,81 @@
+package com.dailycodebuffer.Springboot.tutorial.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Entity
+
+//@DATA is a lombok framework
+@Data
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long departmentId;
+
+    @NotBlank(message = "Please enter Department Name")
+
+
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+
+//    public Long getDepartmentId() {
+//        return departmentId;
+//    }
+//
+//    public void setDepartmentId(Long departmentId) {
+//        this.departmentId = departmentId;
+//    }
+//
+//    public String getDepartmentName() {
+//        return departmentName;
+//    }
+//
+//    public void setDepartmentName(String departmentName) {
+//        this.departmentName = departmentName;
+//    }
+//
+//    public String getDepartmentAddress() {
+//        return departmentAddress;
+//    }
+//
+//    public void setDepartmentAddress(String departmentAddress) {
+//        this.departmentAddress = departmentAddress;
+//    }
+//
+//    public String getDepartmentCode() {
+//        return departmentCode;
+//    }
+//
+//    public void setDepartmentCode(String departmentCode) {
+//        this.departmentCode = departmentCode;
+//    }
+//
+//    public Department(Long departmentId, String departmentName, String departmentAddress, String departmentCode) {
+//        this.departmentId = departmentId;
+//        this.departmentName = departmentName;
+//        this.departmentAddress = departmentAddress;
+//        this.departmentCode = departmentCode;
+//    }
+//
+//    public Department() {
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Department{" +
+//                "departmentId=" + departmentId +
+//                ", departmentName='" + departmentName + '\'' +
+//                ", departmentAddress='" + departmentAddress + '\'' +
+//                ", departmentCode='" + departmentCode + '\'' +
+//                '}';
+//    }
+
+
+    //We use LOMBOK thats why we comment above code
+}
